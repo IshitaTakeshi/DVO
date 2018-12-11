1f9a1e74f1c392b30526da919f64dd4347cd756b
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-12-12 02:51:59 +0900

    Separate 'compute_pixel_coordinates' into the outside of the class

--- motion_estimation/visual_odometry.py
+++ motion_estimation/visual_odometry.py
@@ -298,6 +298,21 @@ def calc_image_gradient(image):
     return np.vstack([dx.flatten(), dy.flatten()]).T
 
 
+def compute_pixel_coordinates(image_shape):
+    # TODO accelerate
+
+    height, width = image_shape[0:2]
+    pixel_coordinates = np.array(
+        [(x, y) for x in range(width) for y in range(height)]
+    )
+    # pixel_coordinates = np.meshgrid(
+    #     np.arange(height),
+    #     np.arange(width)
+    # )
+    # pixel_coordinates = np.array(pixel_coordinates)
+    return pixel_coordinates
+
+
 class VisualOdometry(object):
     def __init__(self, camera_parameters,
                  reference_image, reference_depth,
@@ -311,21 +326,11 @@ class VisualOdometry(object):
         self.camera_parameters = camera_parameters
         self.image_gradient = calc_image_gradient(reference_image)
 
-    def compute_pixel_coordinates(self, image_shape):
-        height, width = image_shape[0:2]
-        pixel_coordinates = np.array([(x, y) for x in range(width) for y in range(height)])
-        # pixel_coordinates = np.meshgrid(
-        #     np.arange(height),
-        #     np.arange(width)
-        # )
-        # pixel_coordinates = np.array(pixel_coordinates)
-        return pixel_coordinates
-
     # @profile
     def compute_jacobian(self, depth_map, g):
         # S.shape = (n_image_pixels, 3)
 
-        pixel_coordinates = self.compute_pixel_coordinates(depth_map.shape)
+        pixel_coordinates = compute_pixel_coordinates(depth_map.shape)
 
         S = inverse_projection(
             self.camera_parameters,
