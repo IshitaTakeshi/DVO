Make jacobian calculation more clear. Fix a bug that image gradient is calculated on 'I0'

motion_estimation/visual_odometry.py
@@ -31,10 +31,11 @@ def compute_mask(camera_parameters, coordinates, image_shape):
 def compute_jacobian(camera_parameters, image, depth_map, g):
     # S.shape = (n_image_pixels, 3)
 
-    image_gradient = calc_image_gradient(image)
 
+def compute_jacobian(camera_parameters, image_gradient, depth_map, g):
     pixel_coordinates = compute_pixel_coordinates(depth_map.shape)
 
+    # S.shape = (n_image_pixels, 3)
     S = inverse_projection(
         camera_parameters,
         pixel_coordinates,
@@ -45,24 +46,32 @@ def compute_jacobian(camera_parameters, image, depth_map, g):
     G = transform(g, S)
 
     # M.shape = (12, n_pose_parameters)
+    # convert xi -> stack(dg)
     M = jacobian_rigid_motion(g)
-    # U.shape = (n_3dpoints, 3, 12)
+
+    # U.shape = (n_image_pixels, 3, 12)
+    # convert stack(dg) -> dG
     U = jacobian_transform(G)
-    # V.shape = (n_3dpoints, 2, 3)
+
+    # V.shape = (n_image_pixels, 2, 3)
+    # convert dG -> dpi
     V = jacobian_projections(camera_parameters, G)
 
-    # Equivalent to J = np.einsum('kli,ij->klj', U, M)
-    # J.shape = (n_3dpoints, 3, n_pose_parameters)
-    J = np.tensordot(U, M, axes=(2, 0))
-
-    # Equivalent to J = np.einsum('ilj,ijk->lk', V, J)
-    # J.shape = (2, n_pose_parameters)
-    J = np.tensordot(V, J, axes=((0, 2), (0, 1)))
-
     # W.shape = (n_image_pixels, 2)
+    # convert dpi -> dI
     W = image_gradient
 
-    J = np.dot(W, J)  # (n_image_pixels, n_pose_parameters)
+    # J.shape = (n_image_pixels, 3, n_pose_parameters)
+    # convert xi -> dG
+    J = np.tensordot(U, M, axes=(2, 0))
+
+    # J.shape = (n_image_pixels, 2, n_pose_parameters)
+    # convert xi -> dpi
+    J = np.einsum('ijk,ikl->ijl', V, J)
+
+    # J.shape = (n_image_pixels, n_pose_parameters)
+    J = np.einsum('ij,ijk->ik', W, J)
+
     return J
 
 
@@ -106,13 +115,13 @@ class VisualOdometry(object):
         return xi
 
     def estimate_in_layer(self, I0, I1, D0, xi):
-        # print(I0.shape, I1.shape, D0.shape)
         y = I1 - I0
         y = y.flatten()
 
         g = transformation_matrix(xi)
-        J = compute_jacobian(self.camera_parameters, I0, D0, g)
-        print("power(J, 2).sum()", np.power(J, 2).sum())
+
+        gradient = calc_image_gradient(I1)
+        J = compute_jacobian(self.camera_parameters, gradient, D0, g)
 
         xi, residuals, rank, singular = np.linalg.lstsq(J, -y, rcond=None)
         # diff = np.dot(J, xi) + y