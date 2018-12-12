9f6932c303d50d1ecb217415d6b23284e51ce547
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-12-13 03:58:37 +0900

    Add a function to generate a twist

--- motion_estimation/twist.py
+++ motion_estimation/twist.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 
-def skew_matrix(v):
+def skew_matrix(k):
     return np.array([
-        [0, -v[2], v[1]],
-        [v[2], 0, -v[0]],
-        [-v[1], v[0], 0]
+        [0, -k[2], k[1]],
+        [k[2], 0, -k[0]],
+        [-k[1], k[0], 0]
     ])
+
+
+def twist(k):
+    K = np.empty((4, 4))
+    K[0:3, 0:3] = skew_matrix(k[:3])
+    K[0:3, 3] = k[3:]
+    K[3] = 0
+    return K
---
+++ tests/test_twist.py
@@ -0,0 +1,34 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from numpy.testing import assert_array_equal
+import numpy as np
+
+from motion_estimation.twist import skew_matrix, twist
+
+
+def test_skew_matrix():
+    GT = np.array([
+        [0, -3, 2],
+        [3, 0, -1],
+        [-2, 1, 0]
+    ])
+
+    assert_array_equal(skew_matrix([1, 2, 3]), GT)
+
+
+def test_twist():
+    GT = np.array([
+        [0, -3, 2, 4],
+        [3, 0, -1, 5],
+        [-2, 1, 0, 6],
+        [0, 0, 0, 0]
+    ])
+
+    assert_array_equal(twist([1, 2, 3, 4, 5, 6]), GT)
+
+
+test_skew_matrix()
+test_twist()
