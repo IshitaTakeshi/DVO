83c9a7a300f721cadc885a0b42c729f3a5d4083f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-12-16 02:50:10 +0900

    Fix a bug that the coordinate order is reversed ((y, x) -> (x, y)).

--- motion_estimation/coordinates.py
+++ motion_estimation/coordinates.py
@@ -1,16 +1,28 @@
 import numpy as np
 
+
 def compute_pixel_coordinates(image_shape):
-    # TODO accelerate
+    """
+
+    Example:
+	For array of shape (3, 2) (if it was an image, height=3 and width=2),
+
+	>>> coordinates.compute_pixel_coordinates((3, 2))
+	array([[0, 0],
+	       [1, 0],
+	       [0, 1],
+	       [1, 1],
+	       [0, 2],
+	       [1, 2]])
+
+    """
 
     height, width = image_shape[0:2]
-    pixel_coordinates = np.array(
-        [(x, y) for x in range(width) for y in range(height)]
-    )
-    # pixel_coordinates = np.meshgrid(
-    #     np.arange(height),
-    #     np.arange(width)
-    # )
-    # pixel_coordinates = np.array(pixel_coordinates)
-    return pixel_coordinates
 
+    xs, ys = np.meshgrid(
+        np.arange(width),
+        np.arange(height)
+    )
+
+    P = np.vstack((xs.flatten(), ys.flatten()))
+    return P.T
---
+++ tests/test_coordinates.py
@@ -0,0 +1,35 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from numpy.testing import assert_array_equal
+import numpy as np
+
+from motion_estimation.coordinates import compute_pixel_coordinates
+
+
+def test_compute_pixel_coordinates():
+    GT = np.array([
+    #    x  y
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [0, 2],
+        [1, 2],
+        [2, 2],
+        [0, 3],
+        [1, 3],
+        [2, 3]
+    ])
+
+    height = 4
+    width = 3
+    assert_array_equal(compute_pixel_coordinates((height, width)), GT)
+
+    compute_pixel_coordinates((height, width))
+
+test_compute_pixel_coordinates()
