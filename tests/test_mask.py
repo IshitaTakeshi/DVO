410e9b273abf6dd71b25a5b76b72d6b0e2bd5682
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-12-16 00:00:21 +0900

    Add a test for mask computation

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -6,7 +6,7 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from motion_estimation.mask import is_in_rage
+from motion_estimation.mask import is_in_rage, compute_mask
 
 
 def test_is_in_rage():
@@ -31,8 +31,9 @@ def test_is_in_rage():
 
     image_shape = (15, 13)  # (height, width)
 
-    # cannot be y > 14.0, x > 12.0
-    # because in that case map_coordinates returns nan
+    # x and y have to be in range
+    # 0 <= x <= 12.0, 0 <= y <= 14.0 respectively
+    # otherwise map_coordinates returns nan
 
     GT = np.array([
         [0.0, 0.0],
@@ -49,7 +50,31 @@ def test_is_in_rage():
 
 
 def test_compute_mask():
-    pass
+    depth_map = np.array([
+        # True False False True
+        [3.1, 0.0, -0.1, 0.4],
+        # True True False True
+        [2.2, 2.0, -0.8, 0.8]
+    ])
+
+    # depth_map.shape == (2, 4)
+    # therefore x and y have to be in range
+    # 0 <= x <= 3.0, 0 <= y <= 1.0 respectively
+    pixel_coordinates = np.array([
+        # False True True False
+        [0.0, -0.2], [0.0, 0.0], [0.8, 0.2], [1.2, 1.4],
+        # False True True False
+        [3.1, 2.0], [3.0, 1.0], [1.0, 0.0], [1.9, 2.9]
+    ])
+
+    GT = np.array([
+        [False, False, False, False],
+        [False, True, False, False]
+    ])
+
+    mask = compute_mask(depth_map, pixel_coordinates)
+
+    assert_array_equal(mask, GT)
 
 
 test_is_in_rage()
