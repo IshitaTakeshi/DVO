00c27f14d206befda774e528ad81d76c2ab45530
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-12-15 14:12:17 +0900

    Plot results

--- examples/app.py
+++ examples/app.py
@@ -13,6 +13,9 @@ from scipy.ndimage import map_coordinates
 from matplotlib import pyplot as plt
 
 from motion_estimation import VisualOdometry, CameraParameters
+from motion_estimation.projection import reprojection
+from motion_estimation.rigid import transformation_matrix
+
 
 image_root = Path("dataset", "20fps_images_archieve")
 depth_root = Path("dataset", "20fps_real_GT_archieve")
@@ -28,7 +31,6 @@ def load(frame):
     path = Path(depth_root, prefix + ".depth")
     depth = np.loadtxt(str(path))
     depth = depth.reshape(image.shape[:2])
-
     return image, depth
 
 
@@ -37,67 +39,47 @@ def approximate_camera_matrix(image_shape):
     return CameraParameters(focal_length=W, offset=[W/2, H/2])
 
 
-from motion_estimation.visual_odometry import (
-    compute_pixel_coordinates, inverse_projection,
-    rigid_transformation, transform, projection)
+def warp(camera_parameters, image, depth, g):
+    P = reprojection(camera_parameters, depth, g)
 
-
-def reprojection(camera_parameters, depth_map, xi):
-    pixel_coordinates = compute_pixel_coordinates(depth_map.shape)
-
-    S = inverse_projection(
-        camera_parameters,
-        pixel_coordinates,
-        depth_map.flatten()
-    )
-
-    g = rigid_transformation(-xi)
-    print(g)
-    G = transform(g, S)
-
-    P = projection(camera_parameters, G)
-    return P
-
-
-def warp(camera_parameters, image, depth, xi):
-    P = reprojection(camera_parameters, depth, xi)
-    # P = compute_pixel_coordinates(depth.shape)
     P = P[:, [1, 0]]
     warped = map_coordinates(image, P.T)
     warped = warped.reshape(image.shape[1], image.shape[0]).T
     return warped
 
 
-def plot(image1, image2):
+def plot(current, next_, estimated):
     fig = plt.figure()
 
-    ax = fig.add_subplot(211)
-    ax.imshow(image1)
+    ax = fig.add_subplot(131)
+    ax.imshow(current)
+    ax.set_title("current image")
 
-    ax = fig.add_subplot(212)
-    ax.imshow(image2)
+    ax = fig.add_subplot(132)
+    ax.imshow(next_)
+    ax.set_title("estimated image")
+
+    ax = fig.add_subplot(133)
+    ax.imshow(estimated)
+    ax.set_title("next image")
 
     plt.show()
 
 
-
-
-camera_parameters = CameraParameters(focal_length=1280, offset=0)
+camera_parameters = CameraParameters(focal_length=10, offset=0)
 
 current_image, current_depth = load(1)
-next_image, next_depth = load(2)
+next_image, next_depth = load(3)
+current_depth = current_depth
+next_depth = next_depth
+vo = VisualOdometry(camera_parameters,
+                    current_image, current_depth, next_image)
+motion = vo.estimate_motion(n_coarse_to_fine=8)
 
-vo = VisualOdometry(
-    camera_parameters,
-    current_image, current_depth,
-    next_image, next_depth
-)
-motion = vo.estimate_motion()
+print("motion")
+print(transformation_matrix(motion))
 
-estimated_image = warp(camera_parameters,
-                       current_image, current_depth, motion)
+g = transformation_matrix(-motion)
+estimated_image = warp(camera_parameters, current_image, current_depth, g)
 
-plot(current_image, estimated_image)
-current_image, current_image = next_image, next_depth
-
-print(motion)
+plot(current_image, estimated_image, next_image)
