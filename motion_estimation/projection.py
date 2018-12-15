7817c7e89d7c1ff2110656fdcfc2e1617e060fc5
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-12-16 02:53:37 +0900

    Fix a bug that the format of 'mask' and 'warped_image' didn't match

--- motion_estimation/projection.py
+++ motion_estimation/projection.py
@@ -2,6 +2,9 @@ import numpy as np
 
 from motion_estimation.coordinates import compute_pixel_coordinates
 from motion_estimation.rigid import transform
+from motion_estimation.mask import compute_mask
+
+from scipy.ndimage import map_coordinates
 
 
 def inverse_projection(camera_parameters, pixel_coordinates, depth):
@@ -49,15 +52,78 @@ def projection(camera_parameters, G):
 
 
 def reprojection(camera_parameters, depth_map, g):
-    pixel_coordinates = compute_pixel_coordinates(depth_map.shape)
+    # 'reprojection' transforms I0 coordinates to corresponding coordinates in I1
 
-    S = inverse_projection(
-        camera_parameters,
-        pixel_coordinates,
-        depth_map.flatten()
-    )
+    # 'P' has pixel coordinates in I1 coordinate system, but each pixel
+    # coordinate is corresponding to the one in I0
 
+    P = compute_pixel_coordinates(depth_map.shape)
+
+    S = inverse_projection(camera_parameters, P, depth_map.flatten())
     G = transform(g, S)
 
-    P = projection(camera_parameters, G)
-    return P
+    Q = projection(camera_parameters, G)
+
+    mask = compute_mask(depth_map, Q)
+
+    # We need to convert 'mask' to the same format as images for convenience
+
+    # Here, mask is in the format below:
+    # mask = [
+    #     v(x0, y0)  v(x1, y0) ... v(xn, y0)
+    #     v(x0, y1)  v(x1, y1) ... v(xn, y1)
+    #     ...
+    #     v(x0, yj)  v(x1, yj) ... v(xn, yj)
+    #     ...
+    #     v(x0, ym)  v(x1, ym) ... v(xn, ym)
+    # ]
+    # where v(x, y) <- {True, False} is a mask at coordinate (x, y)
+
+    mask = mask.reshape(depth_map.shape)
+
+    return Q, mask
+
+
+def warp(camera_parameters, I1, D0, g):
+    # this function samples pixels in I1 and project them to
+    # I0 coordinate system
+
+    # 'reprojection' transforms I0 coordinates to
+    # the corresponding coordinates in I1
+
+    # 'P' has pixel coordinates in I1 coordinate system, but each pixel
+    # coordinate is corresponding to the one in I0
+    # Therefore image pixels sampled by 'P' represents I1 transformed into
+    # I0 coordinate system
+
+    # 'g' describes the transformation from I0 coordinate system to
+    # I1 coordinate system
+
+    P, mask = reprojection(camera_parameters, D0, g)
+
+    height, width = D0.shape
+
+    # Because 'map_coordinates' requires indices of
+    # [row, column] order, the 2nd axis have to be reversed
+    # so that it becomes [y, x]
+
+    P = P[:, [1, 0]].T
+
+    # Here,
+    # P = [
+    #     [y0 y0... y0 ... yi yi... yi ... ym ym ... ym]
+    #     [x0 x1... xn ... x0 x1... xn ... x0 x1 ... xn]
+    # ]
+
+    # sample pixel sequences from the given image I1
+    # warped_image = [
+    #     I[y0, x0]  I[y0, x1]  ...  I[y0, xn]
+    #     I[y1, x0]  I[y1, x1]  ...  I[y1, xn]
+    #     ...
+    #     I[ym, x0]  I[ym, x1]  ...  I[ym, xn]
+    # ]
+
+    warped_image = map_coordinates(I1, P, mode="constant", cval=np.nan)
+    warped_image = warped_image.reshape(height, width)
+
+    return warped_image, mask
