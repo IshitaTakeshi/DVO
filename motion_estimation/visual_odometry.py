109cfb4dc6ea01fb86eb2411d573ef3ab2e2f23f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-01-06 02:50:04 +0900

    Replace the update process with the one proposed by Kerl (2012)

--- motion_estimation/visual_odometry.py
+++ motion_estimation/visual_odometry.py
@@ -30,7 +30,8 @@ def compute_weights(r, nu=5, n_iter=10):
 
 
 class VisualOdometry(object):
-    def __init__(self, camera_parameters, I0, D0, I1):
+    def __init__(self, camera_parameters, I0, D0, I1,
+                 epsilon=5e-10, max_iter=100):
         """
         """
 
@@ -39,6 +40,9 @@ class VisualOdometry(object):
         self.I0 = I0
         self.D0 = D0
         self.I1 = I1
+
+        self.epsilon = epsilon
+        self.max_iter = max_iter
 
         self.camera_parameters = camera_parameters
 
@@ -54,47 +58,60 @@ class VisualOdometry(object):
         # if shape[0] * shape[1] < 6 (number of pose parameters)
         return shapes[::-1]
 
+    def estimate_in_layer(self, I0, D0, I1, G):
+        previous_error = np.inf
+
+        for k in range(self.max_iter):
+            DG, current_error = self.calc_pose_update(I0, D0, I1, G)
+
+            print("k: {}".format(k))
+            print(" previous_error: {}".format(previous_error))
+            print(" current_error : {}".format(current_error))
+
+            if current_error > previous_error:
+                break
+
+            if abs(current_error - previous_error) < self.epsilon:
+                break
+
+            G = G.dot(DG)
+
+            previous_error = current_error
+        return G
+
     def estimate_motion(self, n_coarse_to_fine=5,
-                        initial_estimate=None):
-        if initial_estimate is None:
-            initial_estimate = np.zeros(n_pose_parameters)
-
-        xi = initial_estimate
-
+                        initial_pose=np.eye(4)):
+        G = initial_pose
         for shape in self.image_shapes(n_coarse_to_fine):
-            xi = self.estimate_in_layer(
+            G = self.estimate_in_layer(
                 resize(self.I0, shape),
                 resize(self.D0, shape),
                 resize(self.I1, shape),
-                xi
+                G
             )
+        return G
 
-        return xi
+    def calc_pose_update(self, I0, D0, I1, G, min_depth=1e-4):
+        warped, mask = warp(self.camera_parameters, I1, D0, G)
+        mask = np.logical_and(mask, (D0 > min_depth))
 
-    def estimate_in_layer(self, I0, D0, I1, xi):
-        g = transformation_matrix(xi)
-        gradient = calc_image_gradient(I1)
+        r = (I0[mask] - warped[mask]).flatten()
 
-        # Transform each pixel of I1 to I0 coordinates
+        # image_gradient.shape == (n_image_pixels, 2)
+        image_gradient = calc_image_gradient(I1)[mask.flatten()]
 
-        warped, mask = warp(self.camera_parameters, I1, D0, g)
+        # JW.shape == (n_image_pixels, 2, 6)
+        JW = calc_warping_jacobian(self.camera_parameters, D0, G, mask)
 
-        J = compute_jacobian(self.camera_parameters, gradient, D0, g)
+        # J.shape == (n_image_pixels, 6)
+        J = np.einsum('ij,ijk->ik', image_gradient, JW)
 
-        assert(mask.shape == warped.shape == I0.shape)
+        weights = compute_weights_turkey(r)
+        # weights = compute_weights_student_t(r)
 
-        y = I0.flatten() - warped.flatten()  # comparison on t0 coordinates
+        xi = solve_linear_equation(J, r, weights)
+        DG = transformation_matrix(xi)
 
-        y = y[mask.flatten()]
-        J = J[mask.flatten()]
+        error = calc_error(r, weights)
 
-        # NOTE these asserts should be slow
-        assert(not np.isnan(y).any())
-        assert(not np.isnan(J).any())
-
-        weights = compute_weights(y)
-        JW = J.T * weights
-        L = np.dot(JW, J)
-        xi = -np.linalg.inv(L).dot(JW).dot(y)
-        # xi, residuals, rank, singular = np.linalg.lstsq(J, -y, rcond=None)
-        return xi
+        return DG, error
