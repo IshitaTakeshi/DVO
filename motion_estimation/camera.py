e14245de180ca81979730a1818d8bc031f19a07a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-01-06 02:50:56 +0900

    Remove the camera matrix from 'CameraParameters'

--- motion_estimation/camera.py
+++ motion_estimation/camera.py
@@ -8,11 +8,5 @@ class CameraParameters(object):
         ox, oy = to_tuple_if_scalar(offset)
         fx, fy = to_tuple_if_scalar(focal_length)
 
-        self.matrix = np.array([
-            [fx, 0, -ox],
-            [0, fy, -oy],
-            [0, 0, 1]
-        ])
-
         self.focal_length = np.array([fx, fy])
         self.offset = np.array([ox, oy])
