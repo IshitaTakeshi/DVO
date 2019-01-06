407d0d0fdb5370d92097fc6323361d4799d786c2
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-01-06 18:09:10 +0900

    Fix a bug that the order of translation and rotatin in the pose parameter is wrong

--- motion_estimation/rigid.py
+++ motion_estimation/rigid.py
@@ -1,12 +1,21 @@
 import numpy as np
 from motion_estimation.twist import cross_product_matrix
 
+# Reference:
+# Eade Ethan. "Lie groups for 2d and 3d transformations."
+# http://ethaneade.com/lie.pdf
+# See Section 3.2 Exponential Map
+
+
+# threshold to use an approximated form
+epsilon = 1e-6
+
 
 def rodrigues(omega):
     I = np.eye(3)
     theta = np.linalg.norm(omega)
 
-    if np.isclose(theta, 0):
+    if theta < epsilon:  # since theta = norm(omega) >= 0
         return I
 
     omega = omega / theta
@@ -32,27 +41,28 @@ def transform(G, P):
     return P.T + G[0:3, 3]
 
 
-def transformation_matrix(v):
-    omega, t = v[:3], v[3:]
+def transformation_matrix(xi):
+    v, omega = xi[:3], xi[3:]
 
-    R = rodrigues(omega)
-
-    theta = np.linalg.norm(omega)
     K = cross_product_matrix(omega)
     I = np.eye(3)
+    R = rodrigues(omega)
 
-    if np.isclose(theta, 0):
-        R = V = rodrigues(omega)
+    theta2 = np.dot(omega, omega)
+    theta = np.sqrt(theta2)
+
+    if theta < epsilon:  # since theta = norm(omega) >= 0
+        # teylor expansion of V
+        V = I + K / 2.0 + np.dot(K, K) / 6.0
     else:
-        # TODO not necessary to square
-        V = I + ((1 - np.cos(theta)) / pow(theta, 2) * K +
-                 (theta - np.sin(theta)) / pow(theta, 3) * np.dot(K, K))
-        R = I + (np.sin(theta) / theta * K +
-                 (1 - np.cos(theta)) / pow(theta, 2) * np.dot(K, K))
+        theta3 = theta2 * theta
+        V = (I + (1 - np.cos(theta)) / theta2 * K +
+             (theta - np.sin(theta)) / theta3 * np.dot(K, K))
 
-    g = np.empty((4, 4))
-    g[0:3, 0:3] = R
-    g[0:3, 3] = np.dot(V, t)
-    g[3, 0:3] = 0
-    g[3, 3] = 1
-    return g
+    G = np.empty((4, 4))
+    G[0:3, 0:3] = R
+    G[0:3, 3] = np.dot(V, v)
+    # G[0:3, 3] = np.dot(I-R, K.dot(v)) + np.outer(omega, omega).dot(v) * theta
+    G[3, 0:3] = 0
+    G[3, 3] = 1
+    return G
