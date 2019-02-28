3e369992c05edf25704043eec2927bd13ffab45d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-02-28 09:32:50 +0000

    Rename 'b' -> 'beta'

--- tadataka/weights.py
+++ tadataka/weights.py
@@ -15,10 +15,10 @@ def compute_weights_student_t(r, nu=5, n_iter=10):
     return np.sqrt((nu + 1) / (nu + s / variance))
 
 
-def tukey(x, b=4.6851):
+def tukey(x, beta):
     w = np.zeros(x.shape)
-    mask = np.abs(x) <= b
-    w[mask] = np.power(1 - np.power(x[mask] / b, 2), 2)
+    mask = np.abs(x) <= beta
+    w[mask] = np.power(1 - np.power(x[mask] / beta, 2), 2)
     return w
 
 
@@ -26,8 +26,7 @@ def median_absolute_deviation(x):
     return np.median(np.abs(x - np.median(x)))
 
 
-def compute_weights_tukey(r, b=4.6851):
+def compute_weights_tukey(r, beta=4.6851):
     # Equation 4.28 in the paper
     sigma_mad = median_absolute_deviation(r)
-    return tukey(r / sigma_mad)
-
+    return tukey(r / sigma_mad, beta)
