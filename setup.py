baeeda60a3e698f9f44c8643e075fbd33363a0c3
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-02-28 07:08:18 +0000

    Use 'setup.py' for version control

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tadataka',
+    description='A high-level Python Visual SLAM package',
+    url='http://github.com/IshitaTakeshi/Tadataka',
+    author='Takeshi Ishita',
+    author_email='ishitah.takeshi@gmail.com',
+    license='MIT',
+    packages=['tadataka'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'scikit-image',
+        'tqdm',
+        'pytest'
+    ]
+)
